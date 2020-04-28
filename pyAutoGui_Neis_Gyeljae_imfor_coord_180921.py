6dec4768867dd93fc628c275fb6c6d828cde7a5c
951237 <951237@gmail.com>
2020-04-28 17:00:22 +0900

    Doc : Update - 2019년 개선 버전

--- pyAutoGui_Neis_Gyeljae_imfor_coord_180921.py
+++ pyAutoGui_Neis_Gyeljae_imfor_coord_180921.py
@@ -18,6 +18,7 @@ def waitWindow(p_x, p_y, p_r, p_g, p_b):
     i = 1
     while True:
         if pa.pixelMatchesColor(p_x, p_y, (p_r, p_g, p_b)) == True:  # 수덕원 예약 아이콘 좌표 및 색상코드
+            print('결재창 찾음!')
             break
         print('%s번째 시도중...' %(i))
         i += 1
@@ -27,8 +28,9 @@ def waitWindow(p_x, p_y, p_r, p_g, p_b):
 def findWindow():
     all = pa.getWindows()
     for i in all:
-        if 'http://bms.ken.go.kr/?USERID=driver' in i:
+        if 'https://klef.goe.go.kr/?USERID=driver' in i:
             r_window = i
+            print('결재창 여부 확인~')
         else:
             continue
     return r_window
@@ -37,6 +39,7 @@ def findWindow():
 def getPosition(p_win):
     pa.getWindow(p_win).set_foreground()
     result = pa.getWindow(p_win).get_position()
+    print('윈도우 위치값 확인!')
     return result
 
 #윈도우내에서 일치하는 이미지 검색
@@ -46,7 +49,7 @@ def imgSearchClick(p_imgName):
     pa.click(imgLocationX, imgLocationY, interval=0.5, pause=0.5)
 
 print('결재창 찾는중...')
-waitWindow(662,315,240,240,240)
+waitWindow(700,200,44,133,191) # 앞의 700, 200은 x, y 위치값 / 뒤의 44, 133, 191은 rgb값임
 
 #창의 색깔이 바뀜
 print('결재정보를 찾는 중...')
@@ -58,36 +61,78 @@ print('창의 좌표 : %s' %(str(r_region)))
 #창의 좌측 상단의 X좌표와 Y좌표를 변수로 지정
 leftX, leftY = r_region[0:2]
 
-def clickIwant(_x,_y):
-    pa.click(leftX + _x,leftY + _y,clicks=1,interval=0.25)
+def clickIwant(좌표):
+    pa.click(leftX + 좌표[0],leftY + 좌표[1],clicks=1,interval=0.25)
+    print('딸깍!')
     time.sleep(0.75)
 
 print('결재정보 선택... ')
-clickIwant(67,124)
+결재정보 = [57,122]
+clickIwant(결재정보)
 
 #창의 색이 바뀜
 print('과제카드 찾는 중....')
 print('돋보기 선택....')
-clickIwant(784,251)
+결재정보_돋보기 = [775,249]
+clickIwant(결재정보_돋보기)
 print('정보 단위과제 선택....')
-clickIwant(302,428)
+select = int(input('''
+단위과제를 선택하시오
+1. 과제카드_과학교과
+2. 과제카드_정보교과
+3. 과제카드_개인정보
+4. 과제카드_정보기기관리
+5. 과제카드_보안관리
+6. 과제카드_홈페이지
+7. 과제카드_안전공제
+8. 과제카드_연수관리
+9. 과제카드_교육과정
+'''))
+pa.hotkey('alt', 'tab')
+
+if select == 1:
+    과제카드 = [200,475] # 과학교과
+elif select == 2:
+    과제카드 = [200,450] # 정보교과
+elif select == 3:
+    과제카드 = [200,325] # 개인정보
+elif select == 4:
+    과제카드 = [200,400] # 정보기기관리
+elif select == 5:
+    과제카드 = [200,350] # 보안관리
+elif select == 6:
+    과제카드 = [200,375] # 홈페이지
+elif select == 7:
+    과제카드 = [200,425] # 안전공제
+elif select == 8:
+    과제카드 = [200,500] # 연수관리
+elif select == 9:
+    과제카드 = [200,525] # 교육과정
+
+time.sleep(1)
+clickIwant(과제카드)
+
 print('확인 클릭....')
-clickIwant(682,98)
+과제카드_확인 = [687,175]
+clickIwant(과제카드_확인)
 
 #창의 색이 바뀜.
 print('문서처리 선택....')
-clickIwant(412,93)
+문서처리 = [420, 80]
+clickIwant(문서처리)
 
 print('문서처리창 기다리는 중....')
-waitWindow(637,502,248,248,248)
-# time.sleep(3)
-# print('확인 클릭....')
-# clickIwant(781,255)
-# for i in range(2):
-#     time.sleep(4)
-#     pa.hotkey('enter')
-#
-# print('결재 완료')
+
+waitWindow(800,310,44,133,191)
+time.sleep(3)
+print('확인 클릭....')
+문서처리_확인 = [770, 230]
+clickIwant(문서처리_확인)
+for i in range(2):
+    time.sleep(4)
+    pa.hotkey('enter')
+
+print('결재 완료')
 
 
 
