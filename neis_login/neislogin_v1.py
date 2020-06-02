6066bdf8af4e1c12ae6815f9ea15895206cdc664
951237 <951237@gmail.com>
2020-06-02 16:31:45 +0900

    Doc : Update - 창 불러오기 대기시간 수정

--- neis_login/neislogin_v1.py
+++ neis_login/neislogin_v1.py
@@ -17,23 +17,22 @@ driver.get(url)
 try:
     driver.forward()
     driver.maximize_window()
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(10)
     print('아이디 입력중...')
     driver.find_element_by_xpath('//*[@id="login_input_area"]/input[1]').send_keys('driver1')
 
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(10)
     driver.find_element_by_xpath('//*[@id="login_input_area"]/input[2]').send_keys(Keys.ENTER)
 
     print('인증서 선택 - 이동식 디스크')
     driver.find_element_by_xpath('//*[@id="kc_cert_position"]/tbody/tr[2]/td[2]/button').click()
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(10)
     driver.find_element_by_xpath('//*[@id="kc_cert_position"]/tbody/tr[2]/td[2]/ul/li[1]/a').click()
 
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(10)
     print('비밀번호를 입력합니다.')
     driver.find_element_by_xpath('//*[@id="kc_content_default"]/table/tbody/tr/td[2]/div/div[1]/table/tbody/tr/td[2]/form/input').click()
     input('Press Enter to continue...')
-
 
 
 except Exception as e:
